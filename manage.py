846bce5a93b67cef1c0f24c73931ae47e263e305
Jordi Burgos <jordimaister@gmail.com>
2012-12-13 20:37:56 +0100

    PyDev Debug para Eclipse

--- manage.py
+++ manage.py
@@ -2,6 +2,16 @@
 import os
 import sys
 
+
+try:
+    import pydevd
+    pydevd.patch_django_autoreload(
+    patch_remote_debugger=True, #Connect to the remote debugger.
+    patch_show_console=True
+    )
+except:
+    pass
+
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orderproject.settings")
 
