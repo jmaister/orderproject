c8313a467a3bf03ec974f4afc1c1029854e19639
jmaister <jordimaister@gmail.com>
2013-04-24 21:32:24 +0200

    Add login_required to pages.

--- order/urls.py
+++ order/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import patterns, url
 from django.contrib import admin
+from django.contrib.auth.decorators import login_required
 from django.views.generic.edit import CreateView, UpdateView
 from django.views.generic.list import ListView
+
 from order import views
 from order.forms import InvoiceCreateView, InvoiceUpdateView
 from order.models import Invoice, Product, Client, Tax
@@ -12,24 +14,24 @@ admin.autodiscover()
 urlpatterns = patterns('',
 
     # Invoice
-    url(r'^invoice/$', ListView.as_view(model=Invoice,), name="invoice_list"),
-    url(r'^invoice/new/$', InvoiceCreateView.as_view(), name="invoice_create"),
-    url(r'^invoice/(?P<pk>\d+)/$', InvoiceUpdateView.as_view(), name="invoice_edit"),
+    url(r'^invoice/$', login_required(ListView.as_view(model=Invoice,)), name="invoice_list"),
+    url(r'^invoice/new/$', login_required(InvoiceCreateView.as_view()), name="invoice_create"),
+    url(r'^invoice/(?P<pk>\d+)/$', login_required(InvoiceUpdateView.as_view()), name="invoice_edit"),
 
     # Product
-    url(r'^product/$', ListView.as_view(model=Product,), name="product_list"),
-    url(r'^product/new/$', CreateView.as_view(model=Product), name="product_create"),
-    url(r'^product/(?P<pk>\d+)/$', UpdateView.as_view(model=Product), name="product_edit"),
+    url(r'^product/$', login_required(ListView.as_view(model=Product,)), name="product_list"),
+    url(r'^product/new/$', login_required(CreateView.as_view(model=Product)), name="product_create"),
+    url(r'^product/(?P<pk>\d+)/$', login_required(UpdateView.as_view(model=Product)), name="product_edit"),
 
     # Client
-    url(r'^client/$', ListView.as_view(model=Client,), name="client_list"),
-    url(r'^client/new/$', CreateView.as_view(model=Client), name="client_create"),
-    url(r'^client/(?P<pk>\d+)/$', UpdateView.as_view(model=Client), name="client_edit"),
+    url(r'^client/$', login_required(ListView.as_view(model=Client,)), name="client_list"),
+    url(r'^client/new/$', login_required(CreateView.as_view(model=Client)), name="client_create"),
+    url(r'^client/(?P<pk>\d+)/$', login_required(UpdateView.as_view(model=Client)), name="client_edit"),
     
     # Tax
-    url(r'^tax/$', ListView.as_view(model=Tax,), name="tax_list"),
-    url(r'^tax/new/$', CreateView.as_view(model=Tax), name="tax_create"),
-    url(r'^tax/(?P<pk>\d+)/$', UpdateView.as_view(model=Tax), name="tax_edit"),
+    url(r'^tax/$', login_required(ListView.as_view(model=Tax,)), name="tax_list"),
+    url(r'^tax/new/$', login_required(CreateView.as_view(model=Tax)), name="tax_create"),
+    url(r'^tax/(?P<pk>\d+)/$', login_required(UpdateView.as_view(model=Tax)), name="tax_edit"),
 
     # JSON
     url(r'^json/product/(\d+)/$', views.json_product),
--- order/views.py
+++ order/views.py
@@ -4,18 +4,22 @@ from django.http import HttpResponse
 from django.template.context import Context
 from django.template.loader import get_template
 from order.models import Product, Invoice, InvoiceItem, Tax
+from django.contrib.auth.decorators import login_required
 
-
+@login_required
 def _json_view(request, clazz, pk):
     result = clazz.objects.filter(pk=pk)
     return HttpResponse(serializers.serialize('json', result), mimetype='application/json')
 
+@login_required
 def json_product(request, pk):
     return _json_view(request, Product, pk)
 
+@login_required
 def json_tax(request, pk):
     return _json_view(request, Tax, pk)
 
+@login_required
 def print_order(request, invoice_id):
     invoice = Invoice.objects.get(id=invoice_id)
     invoiceitems = InvoiceItem.objects.select_related().filter(invoice_id=invoice_id)
